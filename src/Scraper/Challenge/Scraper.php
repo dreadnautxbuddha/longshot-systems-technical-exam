e0921922951a0ce5032b8859b16f2e359d96b70c
Peter Cortez <innov.petercortez@gmail.com>
2024-06-17 01:18:21 +0800

    Refactor
    We have made a WebScraper object here whose primary responsibility is to
    scrape websites. We have made this object to allow us the flexibility of
    scraping potentially, let's say raw HTML files that doesn't necessarily
    come from the web, so we're isolating web scraping from the rest.

--- src/Scraper/Challenge/Scraper.php
+++ src/Scraper/Challenge/Scraper.php
@@ -5,72 +5,132 @@ namespace Dreadnaut\LongshotSystemsTechnicalExam\Scraper\Challenge;
 use DOMDocument;
 use DOMXPath;
 use Dreadnaut\LongshotSystemsTechnicalExam\Scraper\Support;
-use GuzzleHttp\Client;
-use GuzzleHttp\Cookie\SessionCookieJar;
+
 use function implode;
 use function intval;
 use function sprintf;
+use function strrev;
 use function trim;
-use function var_dump;
 
-class Scraper implements Support\Contracts\Scraper
+/**
+ * Scrapes the page that comes after the form at https://challenge.longshotsystems.co.uk/go is submitted.
+ *
+ * The trouble with the submission page comes from the fact that the payload, specifically  `answer` that is required
+ * from the previous page is comprised of the user's answer, plus a string -- which is generated using the initial
+ * answer that you supplied in the input.
+ *
+ * Let's say you are presented with the following numbers:
+ * <code>
+ *     1 6 4 4 2 4 1 8
+ * </code>
+ *
+ * You will input the following:
+ *
+ * <code>
+ *     Answer: 16442418
+ *     Name: test
+ * </code>
+ *
+ * After you hit the `Submit` button, you'll be redirected to a URL that looks like so:
+ * <code>
+ *     /submitgo?answer=164424188142446110094899d4d00e01d399864d1a0b91eca41&name=test
+ * </code>
+ *
+ * You'll notice that the answer is different from what we supplied in the input. We can break it down into four
+ * segments:
+ * 1. `16442418`: This is just the answer
+ * 2. `81424461`: while this one is the palindrome of the answer
+ * 2. `100`: This one has been generated using a specific formula. Refer to {@see Scraper::generateSessionId}
+ * 3. `94899d4d00e01d399864d1a0b91eca41`: This is the associated `data` attribute of the `.number-box` element of `n`
+ *      index which is generated using this formula: `$numbersAsInt % 8`. Here, `$numbersAsInt` is basically the number
+ *      that we have in number one -- which is `16442418`
+ *
+ * @package Dreadnaut\LongshotSystemsTechnicalExam\Scraper\Challenge
+ *
+ * @author  Peter Cortez <innov.petercortez@gmail.com>
+ */
+class Scraper extends Support\Contracts\WebScraper
 {
     /**
      * @inheritDoc
      */
     public function scrape(): Page
     {
-        $jar = new SessionCookieJar('PHPSESSID', true);
-        $guzzle = new Client(['cookies' => $jar]);
-        // 1. First, make a request to the /go page. This will show us the numbers
-        // that we will submit later.
-        $go = $guzzle->get('https://challenge.longshotsystems.co.uk/go');
-        $document = new DOMDocument();
-        $document->loadHTML((string) $go->getBody());
-        $xpath = new DOMXPath($document);
         $numbers = [];
         $data = [];
-        foreach ($xpath->query("//*[contains(@class, 'number-box')]") as $node)
-        {
-            $numbers[] = intval(trim($node->textContent));
-            $data[] = $node->attributes->getNamedItem('data')->nodeValue;
+        foreach (
+            $this->getNumberNodes($this->getDOMDocument('https://challenge.longshotsystems.co.uk/go')) as [
+                $number,
+                $datum
+            ]
+        ) {
+            $numbers[] = $number;
+            $data[] = $datum;
         }
         $numbersAsInt = intval(implode('', $numbers));
-        // 2. We now have the numbers. We can start building the answer. It is comprised
-        // of the answer + a string that is generated from the numbers.
         $index = $numbersAsInt % 8;
-        // $combined = $this->gen($numbersAsInt);
-        $answer = sprintf('%s%s', $this->gen($numbersAsInt), $data[$index]);
-        // var_dump($answer, $numbersAsInt, $this->gen($numbersAsInt), $data[$index]);
-        // var_dump($answer, $numbersAsInt, $this->gen($numbersAsInt), $data[$index]);
-        // die();
-        // $answer = $this->gen($numbersAsInt);
-        // var_dump($numbersAsInt, $index);
-        // die();
-        // $answer = implode('', $numbers);
+        $answer = $this->getAnswer($numbersAsInt, $data[$index]);
 
-        $submitGo = $guzzle->get("https://challenge.longshotsystems.co.uk/submitgo?answer={$answer}&name=test");
-        // var_dump((string) $submitGo->getBody());
-        // die();
-        $submitGoDocument = new DOMDocument();
-        $submitGoDocument->loadHTML((string) $submitGo->getBody());
-        // var_dump($numbers);
-        // die();
-        // echo "<pre>";
-        // print_r($numbers);
-        // die();
-        // Get the numbers first
-        // then make another request to the submitgo endpoint like so:
-        // https://challenge.longshotsystems.co.uk/submitgo?answer=1248733333378421100215581794490b86ba2222d0c297c3eb2&name=Peter%20Cortez
-
-        return new Page($submitGoDocument);
+        return new Page(
+            // From what we've seen, the `name` query parameter doesn't really have a "correct" value so we're just
+            // giving it a value of `test`
+            $this->getDOMDocument("https://challenge.longshotsystems.co.uk/submitgo?answer={$answer}&name=test")
+        );
     }
 
-    private function gen(int $numbersAsInt): int
+    /**
+     * Looks for all elements within the document that has a class of `number-box` and returns an array with the first
+     * element containing the number, and the second containing its `data` attribute's value.
+     *
+     * @param DOMDocument $document
+     *
+     * @return iterable
+     */
+    private function getNumberNodes(DOMDocument $document): iterable
     {
-        $reverse = strrev($numbersAsInt);
-        $concat = intval("{$numbersAsInt}{$reverse}");
+        $xpath = new DOMXPath($document);
 
-        return $concat . (($concat % 5) + 99) * ($concat % 5);
+        foreach ($xpath->query("//*[contains(@class, 'number-box')]") as $node) {
+            yield [intval(trim($node->textContent)), $node->attributes->getNamedItem('data')->nodeValue];
+        }
+    }
+
+    /**
+     * Generates an answer from the supplied number and UUID
+     *
+     * @param int    $numbers
+     * @param string $uuid
+     *
+     * @return string
+     */
+    private function getAnswer(int $numbers, string $uuid): string
+    {
+        return sprintf('%s%s%s%s', $numbers, strrev($numbers), $this->generateSessionId($numbers), $uuid);
+    }
+
+    /**
+     * From the answers, a session ID is generated using the reverse of the supplied numbers concatenated with a number
+     * using this formula:
+     * <code>
+     *     $concat = intval("{$numbersAsInt}{$reversedNumbersAsInt}");
+     *
+     *     (($concat % 5) + 99) * ($concat % 5)
+     * </code>
+     *
+     * So here, a value of `16442418` will result in this formula:
+     * <code>
+     *     ((1644241881424461 % 5) + 99) * (1644241881424461 % 5)
+     * </code>
+     * and will be equal to `100`
+     *
+     * @param int $numbersAsInt
+     *
+     * @return int
+     */
+    private function generateSessionId(int $numbersAsInt): int
+    {
+        $concat = sprintf('%d%d', $numbersAsInt, strrev($numbersAsInt));
+
+        return (($concat % 5) + 99) * ($concat % 5);
     }
 }
